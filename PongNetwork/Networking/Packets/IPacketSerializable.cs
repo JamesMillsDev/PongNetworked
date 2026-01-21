5d323adfea487df05b3a12a299b24fdc6374c399
James Mills <jcmills.developer@gmail.com>
2026-01-22 09:07:33 +1100

    Setup submodule for networking

--- PongNetwork/Networking/Packets/IPacketSerializable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pong.Networking.Packets
-{
-	public interface IPacketSerializable
-	{
-		public byte[] Serialize();
-		public void Deserialize(byte[] data);
-		public int GetSize();
-	}
-}
